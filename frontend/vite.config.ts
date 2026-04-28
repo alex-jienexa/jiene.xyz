c5f9a6094b55b02c6e16d83f1d5457536e6f7594
Alex Jienexa <alexjienexa@gmail.com>
2026-04-28 16:38:59 +0300

    Сделать исправления подключения внешнего домена

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,12 @@ import devtools from "solid-devtools/vite";
 
 export default defineConfig({
   plugins: [devtools(), solidPlugin()],
+  preview: {
+    host: true,
+    port: 3000,
+    // Сделать доступ к серверу через внешний порт
+    allowedHosts: ["jiene.462292.xyz"],
+  },
   server: {
     port: 3000,
   },
